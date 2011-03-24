f916430b5b16a981f05a5d4e416838ff61275e16
Kirit Sælensminde <k@kirit.com>
2011-03-25 00:34:11 +0700

    There is now 100% code coverage.

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -37,4 +37,8 @@ class RequestHandler(TestCase):
     def setUp(self):
         self.m = Middleware()
 
+    def test_no_authorization_header(self):
+        r = _Mockrequest(None)
+        u = self.m.process_request(r)
+        self.assertEquals(u, None)
 
