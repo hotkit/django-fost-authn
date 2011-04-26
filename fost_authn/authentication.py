79029457f9de6a9f077d5b9d6dd14ed907da89bf
Kirit Sælensminde <k@kirit.com>
2011-04-26 12:35:14 +0700

    The coverage results now tell us that the authentication backend is being properly called even if the request isn't properly signed.

--- fost_authn/authentication.py
+++ fost_authn/authentication.py
@@ -1,4 +1,9 @@
 
 
 class FostBackend(object):
-    pass
+    def authenticate(self, **kwargs):
+        if kwargs.has_key('request') and kwargs.has_key('key') and kwargs.has_key('hmac'):
+            pass
+
+    def get_user(self, user_id):
+        pass
--- fost_authn_debug/tests/test_fake_client.py
+++ fost_authn_debug/tests/test_fake_client.py
@@ -4,16 +4,30 @@ from django.test.client import Client
 
 
 class TestFakeHTTPClientUnsigned(TestCase):
+    """Tests that don't even try to sign the client requests."""
+
     def setUp(self):
         self.ua = Client()
 
     def test_get_root(self):
         self.ua.get('/debug/')
 
-    def test_get_unsigned(self):
+    def test_get_anonymous(self):
         response = self.ua.get('/debug/anonymous/')
         self.assertEquals(response.status_code, 200)
 
     def test_get_unsigned_404(self):
         response = self.ua.get('/debug/not-a-url/')
         self.assertEquals(response.status_code, 404)
+
+
+class TestFakeHTTPClientMissigned(TestCase):
+    """ These tests sign the client requests, but not in a valid way."""
+
+    def setUp(self):
+        self.ua = Client()
+        old_get = self.ua.get
+        self.ua.get = lambda p, d = {}: old_get(p, d, HTTP_AUTHORIZATION='FOST key:hmac')
+
+    def test_get_root(self):
+        self.ua.get('/debug/')
