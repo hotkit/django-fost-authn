431377d28d341d921747083e085a3bdf897aa423
Kirit Sælensminde <k@kirit.com>
2011-04-26 12:09:38 +0700

    Added some more view tests for the debug module so that we can be sure that we haven't broken any of the standard handling.

--- fost_authn_debug/tests/test_fake_client.py
+++ fost_authn_debug/tests/test_fake_client.py
@@ -7,6 +7,13 @@ class TestFakeHTTPClientUnsigned(TestCase):
     def setUp(self):
         self.ua = Client()
 
+    def test_get_root(self):
+        self.ua.get('/debug/')
+
     def test_get_unsigned(self):
-        response = self.ua.get('/debug/')
+        response = self.ua.get('/debug/anonymous/')
         self.assertEquals(response.status_code, 200)
+
+    def test_get_unsigned_404(self):
+        response = self.ua.get('/debug/not-a-url/')
+        self.assertEquals(response.status_code, 404)
--- fost_authn_debug/views.py
+++ fost_authn_debug/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 
 def root(request):
     return HttpResponse("Debug helper", mimetype='text/plain')
+
+
+def anonymous(request):
+    assert not request.user.is_authenticated()
+    return render_to_response('ok.html')
