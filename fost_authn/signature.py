b15b6d3875a933c3f328e81c0e710d44c7bdb25a
Kirit Sælensminde <kirit@klimt.(none)>
2011-04-27 10:30:07 +0700

    Now calculates the correct signature.

--- fost_authn/signature.py
+++ fost_authn/signature.py
@@ -2,16 +2,22 @@ import hashlib
 import hmac
 
 
-def _sha1_hmac(secret, document):
+def sha1_hmac(secret, document):
     """
         Calculate the Base 64 encoding of the HMAC for the given document.
     """
     return hmac.new(secret, document, hashlib.sha1).digest().encode("base64")[:-1]
 
 
-def fost_hmac_signature(secret, method, path, timestamp):
+def fost_hmac_signature(secret, method, path, timestamp, headers = {}, body = ''):
     """
         Calculate the signature for the given secret and arguments.
     """
-    document = "%s %s\n%s" % (method, path, timestamp)
-    return _sha1_hmac(secret, document)
+    signed_headers, header_values = 'X-FOST-Headers', []
+    for header, value in headers.items():
+        signed_headers += ' ' + header
+        header_values.append(value)
+    document = "%s %s\n%s\n%s\n%s" % (method, path, timestamp,
+        '\n'.join([signed_headers] + header_values), body)
+    signature = sha1_hmac(secret, document)
+    return document, signature, headers
--- fost_authn_debug/tests/test_hmac.py
+++ fost_authn_debug/tests/test_hmac.py
@@ -1,8 +1,25 @@
 from unittest2 import TestCase
 
-from fost_authn.signature import fost_hmac_signature
+from fost_authn.signature import fost_hmac_signature, sha1_hmac
 
 
 class TestSignature(TestCase):
+    document = """GET /\n2011-04-27 03:26:37.625618\nX-FOST-Headers\n"""
+    def test_sha1_hmac(self):
+        signature = sha1_hmac('secret-value', self.document)
+        self.assertEquals(signature, 'Ttq8K3g/jm7sOAVzCN+3k4FVAso=')
+
     def test_get(self):
-        signature = fost_hmac_signature('secret', 'GET', '/', '2010-04-05 14:45:34')
+        document, signature, headers  = \
+            fost_hmac_signature('secret-value', 'GET', '/', '2011-04-27 03:26:37.625618')
+        self.assertEquals(document, self.document)
+        self.assertEquals(signature, 'Ttq8K3g/jm7sOAVzCN+3k4FVAso=')
+
+
+class TestHeaderSignatures(TestCase):
+    def test_get_with_user(self):
+        document, signature, headers = \
+            fost_hmac_signature('secret-value', 'GET', '/', '2011-04-27 03:26:37.625618', {
+                'X-FOST-User': 'admin'})
+        self.assertEquals(document,
+            """GET /\n2011-04-27 03:26:37.625618\nX-FOST-Headers X-FOST-User\nadmin\n""")
