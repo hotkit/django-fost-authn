75300f98018b6f32d570e314d9bb858206f9a3fd
Kirit Sælensminde <k@kirit.com>
2011-05-01 13:12:46 +0700

    The mock request now properly signs headers that it's been asked to sign.

--- fost_authn_debug/tests/mock_request.py
+++ fost_authn_debug/tests/mock_request.py
@@ -10,13 +10,15 @@ class MockRequest(object):
         if authz:
             self.META['HTTP_AUTHORIZATION'] = authz
 
-    def sign(self, key, secret):
+    def sign(self, key, secret, headers = {}):
         if not self.META.has_key('HTTP_X_FOST_TIMESTAMP'):
             self.META['HTTP_X_FOST_TIMESTAMP'] = str(datetime.utcnow())
         if not self.META.has_key('HTTP_X_FOST_HEADERS'):
             self.META['HTTP_X_FOST_HEADERS'] = 'X-FOST-Headers'
-        to_sign = {}
+        for key, value in headers.items():
+            self.META['HTTP_%s' % key.upper().replace('-', '_')] = value
+            self.META['HTTP_X_FOST_HEADERS'] += ' %s' % key
         document, signature, headers = \
             fost_hmac_signature(secret, self.method, self.path,
-                self.META['HTTP_X_FOST_TIMESTAMP'], to_sign, self.raw_post_data)
+                self.META['HTTP_X_FOST_TIMESTAMP'], headers, self.raw_post_data)
         self.META['HTTP_AUTHORIZATION'] = 'FOST %s:%s' % (key, signature)
