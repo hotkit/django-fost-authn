48d80b37e216bd2fdbef9d9f43514a455aa28d3a
Kirit Sælensminde <k@kirit.com>
2011-05-01 13:13:40 +0700

    Ensure that the authentication backend adds the correct signed headers in to the SIGNED dict.

--- fost_authn_debug/tests/test_authentication.py
+++ fost_authn_debug/tests/test_authentication.py
@@ -3,6 +3,7 @@ from unittest2 import TestCase
 import mock
 
 from django.conf import settings
+from django.contrib.auth.models import User
 
 from mock_request import MockRequest
 
@@ -50,3 +51,15 @@ class TestAuthentication(TestCase):
         for key in ['X-FOST-Headers']:
             self.assertTrue(self.request.SIGNED.has_key(key), (key, self.request.SIGNED))
 
+
+    def test_signed_request(self):
+        user, created = User.objects.get_or_create(username='test-user')
+        headers = {'X-FOST-User': user.username}
+        self.request.sign(self.key, self.secret(), headers)
+        key, self.hmac = self.middleware.key_hmac(self.request)
+        with mock.patch('fost_authn.authentication._forbid', self.fail):
+            result = self.backend.authenticate(request = self.request,
+                key = self.key, hmac = self.hmac)
+        self.assertTrue(hasattr(self.request, 'SIGNED'))
+        for key in ['X-FOST-Headers', 'X-FOST-User']:
+            self.assertTrue(self.request.SIGNED.has_key(key), (key, self.request.SIGNED))
