0fb8a1d0192ff9d0836722a877ad9d562e186bf3
Kirit Saelensminde <k@kirit.com>
2011-05-25 17:30:16 +0700

    Now make sure that the server creates the proper document for signing.

--- fost_authn/signature.py
+++ fost_authn/signature.py
@@ -7,7 +7,9 @@ def sha1_hmac(secret, document):
     """
         Calculate the Base 64 encoding of the HMAC for the given document.
     """
-    return hmac.new(secret, document, hashlib.sha1).digest().encode("base64")[:-1]
+    signature = hmac.new(secret, document, hashlib.sha1).digest().encode("base64")[:-1]
+    logging.info("Signature %s for secret %s and document:\n%s", signature, secret, document)
+    return signature
 
 
 def fost_hmac_url_signature(
