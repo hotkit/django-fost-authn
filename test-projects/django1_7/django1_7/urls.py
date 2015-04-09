b7a38adfc040eab62be52cc7823b8cc5f9d70013
Kirit Sælensminde <k@kirit.com>
2015-04-09 10:42:41 +0700

    Load the URLs.

--- test-projects/django1_7/django1_7/urls.py
+++ test-projects/django1_7/django1_7/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'django1_7.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    (r'^debug/', include('fost_authn_debug.urls')),
+
     url(r'^admin/', include(admin.site.urls)),
 )
