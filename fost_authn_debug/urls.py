534b57d6e3e7ce8dc1a75d7cc00220cd9202f9ac
Kirit Sælensminde <k@kirit.com>
2015-04-09 10:44:43 +0700

    Make the URLs compatible with later versions of Django.

--- fost_authn_debug/urls.py
+++ fost_authn_debug/urls.py
@@ -1,4 +1,9 @@
-from django.conf.urls.defaults import *
+try:
+    # This API has changed.
+    from django.conf.urls import patterns
+except ImportError:
+    from django.conf.urls.defaults import patterns
+
 
 urlpatterns = patterns('',
     (r'^$', 'fost_authn_debug.views.root'),
